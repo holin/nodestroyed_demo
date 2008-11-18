d7ff94fa1544c98dd2a15f35b0ce2ec11d3b0dcf
holin <holin@HoLin.local>
2008-11-18 19:18:10 +0800

    add index to  destroyed

--- db/migrate/20081118111317_add_destroyed_to_posts.rb
+++ db/migrate/20081118111317_add_destroyed_to_posts.rb
@@ -1,7 +1,11 @@
 class AddDestroyedToPosts < ActiveRecord::Migration
   def self.up
+    add_column :posts, :destroyed, :boolean, :default => false
+    add_index :posts, [:destroyed]
   end
 
   def self.down
+    remove_column :posts, :destroyed
+    remove_index :posts, [:destroyed]
   end
 end
